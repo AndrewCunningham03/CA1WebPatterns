df53ccde6e960c511bcaab93bccf81289d92dcdd
TobyZedomi <tobyzedo4@gmail.com>
2024-10-09 19:01:24 +0100

    Users class

---
+++ src/main/java/business/Users.java
@@ -0,0 +1,79 @@
+package business;
+
+import java.util.Objects;
+
+public class Users {
+
+
+    private String username;
+    private String email;
+    private String password;
+    private int userType;
+
+    public Users(String username, String email, String password, int userType) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", userType=" + userType +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return Objects.equals(username, users.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+}
+
+
