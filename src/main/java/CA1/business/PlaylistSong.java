07fe6eccc91bd4f82789577a3750f2ed7547bd72
Andrew <Andrewcunningham9911@hotmail.com>
2024-10-10 20:38:27 +0100

    playlistsong class added

--- src/main/java/CA1/business/PlaylistSong.java
+++ src/main/java/CA1/business/PlaylistSong.java
@@ -1,5 +1,37 @@
 package CA1.business;
 
 public class PlaylistSong {
+//playlistName varchar(50) NOT NULL,
+//    songName     varchar(50) NOT NULL,
+    private String playlistName;
+    private String songName;
 
+    public PlaylistSong(String playlistName, String songName) {
+        this.playlistName = playlistName;
+        this.songName = songName;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    @Override
+    public String toString() {
+        return "PlaylistSong{" +
+                "playlistName='" + playlistName + '\'' +
+                ", songName='" + songName + '\'' +
+                '}';
+    }
 }
