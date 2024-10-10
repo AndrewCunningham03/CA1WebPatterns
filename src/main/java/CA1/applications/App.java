d0527a6e7cffb4562e7e6501031458249a9af042
TobyZedomi <tobyzedo4@gmail.com>
2024-10-10 20:37:13 +0100

    Updating App class with adding menu code for view all artist in library

--- src/main/java/CA1/applications/App.java
+++ src/main/java/CA1/applications/App.java
@@ -1,11 +1,15 @@
 package CA1.applications;
 
+import CA1.business.Artist;
+import CA1.persistence.ArtistDao;
+import CA1.persistence.ArtistDaoImpl;
 import CA1.persistence.UserDao;
 import CA1.persistence.UserDaoImpl;
 
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -95,6 +99,58 @@ public class App {
 
             }
 
+        }
+
+
+        int num2 = 0;
+
+        ArtistDao artistDao = new ArtistDaoImpl("database.properties");
+
+        while(num2 != 7){
+            String [] array2 = new String[7];
+
+            array2[0] = "1. View all artist in the library";
+            array2[1] = "2. View all albums for an artist";
+            array2[2] = "3. View all songs in an album";
+            array2[3] = "4. Search for song by title";
+            array2[4] = "5. Search for song by artist";
+            array2[5] = "6. Search for song by album";
+            array2[6] = "7. Exit";
+
+            for (int i = 0; i < array2.length; i++) {
+                System.out.println(array2[i]);
+            }
+
+            System.out.println("Enter number: ");
+            num2 = keyboard.nextInt();
+
+            if (num2 <= 0 || num2 > array2.length) {
+                System.out.println("Number is not on menu");
+            }
+
+            switch (num2){
+
+                case 1:
+
+                    ArrayList<Artist> artists = artistDao.getAllArtist();
+
+                    for (Artist a: artists){
+                        System.out.println("Artist: " +a);
+                    }
+
+                    System.out.println("");
+                    break;
+                case 2 :
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
         }
 
     }
